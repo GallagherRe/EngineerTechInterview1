53c1313d8b3983e42e3787163dde8559c3e3f542
GallagherRe <104138965+GallagherRe@users.noreply.github.com>
2022-04-21 11:51:53 +0100

    Add files via upload

---
+++ ajg-technical-interview/Controllers/SanctionedEntitiesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ajg_technical_interview.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ajg_technical_interview.Controllers
+{
+    [ApiController]
+    [Route("api/sanctioned-entities")]
+    public class SanctionedEntitiesController : ControllerBase
+    {
+        private readonly IDatabaseService _databaseService;
+
+        public SanctionedEntitiesController(IDatabaseService databaseService)
+        {
+            _databaseService = databaseService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetSanctionedEntities()
+        {
+            try
+            {
+                var entities = await _databaseService.GetSanctionedEntitiesAsync();
+                return Ok(entities);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+            
+        }
+
+    }
+}
---
+++ ajg-technical-interview/Models/SanctionedEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ajg_technical_interview.Models
+{
+    public class SanctionedEntity
+    {
+        public Guid Id => Guid.NewGuid();
+        public string Name { get; set; }
+        public string Domicile { get; set; }
+        public bool Accepted { get; set; }
+    }
+}
