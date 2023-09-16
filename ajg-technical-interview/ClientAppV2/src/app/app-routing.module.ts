dede0c4aa0fc5c353216ebb0d43954d0919f7544
Ram Infosystems Limited <55663344+raminfosystems@users.noreply.github.com>
2023-09-16 09:32:28 +0100

    Added correct routes and home component modified.
    Added correct routes and home component modified.

--- ajg-technical-interview/ClientAppV2/src/app/app-routing.module.ts
+++ ajg-technical-interview/ClientAppV2/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './components/counter/counter.component';
+import { HomeComponent } from './components/home/home.component';
+import { SanctionedEntitiesComponent } from './components/sanctioned-entities/sanctioned-entities.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'counter', component: CounterComponent },
+  { path: 'sanctioned-entities', component: SanctionedEntitiesComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
